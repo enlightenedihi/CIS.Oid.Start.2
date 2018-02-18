c03bb261a036f489c282f9db263e6b7f553a55bb
enlightenedihi <enlightenihi@live.com>
2018-02-18 13:43:09 -0800

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CIS.Oid.Start._2
+{
+    static class Program
+    {
+        private static string DNA =
+        "using System;" +
+        "namespace Oid" +
+        "{" +
+        "    class Program" +
+        "    {" +
+        "        static void Main(string[] args)" +
+        "        {" +
+        "        }" +
+        "    }" +
+        "}";
+
+        private static void Main(string[] args)
+        {
+            Console.WriteLine(DNA);
+
+            //  ANALYZE & MODIFICATION
+            ////////////////////////////////////
+
+            //  change signature class
+            int position = DNA.IndexOf("class");
+            DNA = DNA.Insert(position - 1, "private");
+
+            Console.ReadLine();
+        }
+    }
+}
